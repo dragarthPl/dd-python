959bf294df79c0f373c7491ff2b9f80aa447a28a
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-07-11 20:44:08 +0200

    task to implement taking into account shared resource while parallelization

--- src/main/domaindrivers/smartschedule/planning/parallelization/parallel_stages_list.py
+++ src/main/domaindrivers/smartschedule/planning/parallelization/parallel_stages_list.py
@@ -8,13 +8,5 @@ from domaindrivers.smartschedule.planning.parallelization.parallel_stages import
 class ParallelStagesList:
     all: list[ParallelStages]
 
-    @classmethod
-    def empty(cls) -> "ParallelStagesList":
-        return cls([])
-
     def print(self) -> str:
         return " | ".join(map(lambda stages: stages.print(), self.all))
-
-    def add(self, new_parallel_stages: ParallelStages) -> "ParallelStagesList":
-        result: list[ParallelStages] = self.all + [new_parallel_stages]
-        return ParallelStagesList(result)
