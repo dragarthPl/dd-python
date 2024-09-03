62c98160d90ff0b8a2f1b1cbfdd3ce43bc75aa2e
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-09-03 23:00:04 +0200

    implemented capability selector

--- src/main/domaindrivers/smartschedule/allocation/capabilityscheduling/capability_selector.py
+++ src/main/domaindrivers/smartschedule/allocation/capabilityscheduling/capability_selector.py
@@ -1,18 +1,31 @@
+from enum import Enum
+
+from attr import frozen
 from domaindrivers.smartschedule.shared.capability.capability import Capability
 from domaindrivers.utils.serializable import Serializable
 
 
+@frozen
 class CapabilitySelector(Serializable):
-    @staticmethod
-    def can_perform_one_of(capabilities: set[Capability]) -> "CapabilitySelector":
-        return None
+    class SelectingPolicy(Enum):
+        ALL_SIMULTANEOUSLY = 0
+        ONE_OF_ALL = 1
 
-    @staticmethod
-    def can_perform_all_at_the_time(being_an_admin: set[Capability]) -> "CapabilitySelector":
-        return None
+    capabilities: set[Capability]
+    selecting_policy: SelectingPolicy
+
+    @classmethod
+    def can_perform_all_at_the_time(cls, capabilities: set[Capability]) -> "CapabilitySelector":
+        return cls(capabilities, cls.SelectingPolicy.ALL_SIMULTANEOUSLY)
+
+    @classmethod
+    def can_perform_one_of(cls, capabilities: set[Capability]) -> "CapabilitySelector":
+        return cls(capabilities, cls.SelectingPolicy.ONE_OF_ALL)
 
     def can_perform(self, capability: Capability) -> bool:
-        return False
+        return capability in self.capabilities
 
     def can_perform_capabilities(self, capabilities: set[Capability]) -> bool:
-        return False
+        if len(capabilities) == 1:
+            return self.capabilities == capabilities
+        return self.selecting_policy == self.SelectingPolicy.ALL_SIMULTANEOUSLY and self.capabilities == capabilities
