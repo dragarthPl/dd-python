cef0fd6463ff04374a74bd659c912b0759355b92
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-09-30 22:16:20 +0200

    implemented task to implement taking into account shared resource while parallelization

--- src/main/domaindrivers/smartschedule/planning/parallelization/stages_to_nodes.py
+++ src/main/domaindrivers/smartschedule/planning/parallelization/stages_to_nodes.py
@@ -9,8 +9,24 @@ class StagesToNodes:
 
         for i, stage in enumerate(stages):
             result = self.__explicit_dependencies(stage, result)
+            result = self.__shared_resources(stage, stages[i + 1 :], result)
 
         return Nodes(set(result.values()))
+
+    def __shared_resources(self, stage: Stage, with_stages: list[Stage], result: dict[str, Node]) -> dict[str, Node]:
+        for other in with_stages:
+            if not stage.name == other.name:
+                if not stage.resources.isdisjoint(other.resources):
+                    node: Node
+                    if len(other.resources) > len(stage.resources):
+                        node = result.get(stage.name)
+                        node = node.depends_on(result.get(other.name))
+                        result[stage.name] = node
+                    else:
+                        node = result.get(other.name)
+                        node = node.depends_on(result.get(stage.name))
+                        result[other.name] = node
+        return result
 
     def __explicit_dependencies(self, stage: Stage, result: dict[str, Node]) -> dict[str, Node]:
         node_with_explicit_deps: Node = result.get(stage.name)
